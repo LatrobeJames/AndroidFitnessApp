33ff8864226880a40c15a87026cf4e7107807932
Jacobster111 <55086799+Jacobster111@users.noreply.github.com>
2023-05-09 15:38:08 +1000

    Testing Git Commit and Push

--- app/src/main/java/com/example/androidfitnessapp/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/androidfitnessapp/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is dashboard fragment! \n Testing Git Commit and Push!");
     }
 
     public LiveData<String> getText() {
