d3d3f1cb2a518c1c8d88efa1b85e09b6d8f43ff7
LatrobeJames <127180009+LatrobeJames@users.noreply.github.com>
2023-05-15 18:55:43 +1000

    google SSO should be working
    no logout functionality yet.
    
    should go to second screen.
    
    added a lot of dependencies for firebase, google
    
    added a few dependencies for android
    
    reworked entire welcome page code

--- app/src/main/java/com/example/androidfitnessapp/WelcomeActivity.java
+++ app/src/main/java/com/example/androidfitnessapp/WelcomeActivity.java
@@ -6,23 +6,79 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import androidx.annotation.Nullable;
+import com.google.android.gms.common.api.ApiException;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+
+
     Button startButton;
+    GoogleSignInOptions googleSignInOptions;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        //nameTextView = findViewById(R.id.nameTextView);
+       // signOutButton = findViewById(R.id.signOutButton);
+
+
         startButton = findViewById(R.id.start_button);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
-                startActivity(intent);
+                GoogleSignInHelper();
+                //Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                //startActivity(intent);
             }
         });
+
+
+        }
+
+    private void GoogleSignInHelper() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent, 100);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                task.getResult(ApiException.class);
+                //not sur eif we need to finish our current one
+                //finish();
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                //AuthedHomeActivity();
+            } catch (ApiException e) {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+   // private void AuthedHomeActivity() {
+   //     finish();
+   //     Intent intent = new Intent(getApplicationContext(), AuthedHomeActivity.class);
+   //     startActivity(intent);
+   // }
 }
